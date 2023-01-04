4a44ad9f6a19989cb7fb6458ae4290ca24a01ada
Pedro Henriques <pedro@pedrojhenriques.com>
2023-01-04 11:55:29 +0000

    updated initial files

--- cli/lint.sh
+++ cli/lint.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 set -e
 
-docker run --rm -v "${PWD}/":"/usr/src/app/" golangci/golangci-lint:v1.50 /bin/sh -c "go mod tidy && golangci-lint run -v";
+PULL_FLAG="";
+
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    --pull) PULL_FLAG="--pull always"; shift 1;;
+
+    -*) echo "unknown option: $1" >&2; exit 1;;
+    *) DIRS="$DIRS $1"; shift 1;;
+  esac
+done
+
+docker run --rm -v "${PWD}/":"/usr/src/app/" golangci-lint:latest /bin/sh -c "go mod tidy && golangci-lint run -v";
--- cli/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-
-# Compile and run the application
-go run ./main.go;
